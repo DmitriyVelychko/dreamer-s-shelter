36bc1a0829a0efe9c9e04e31b85185c74a5df79c
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2016-12-04 15:57:22 +0200

    Change project structure , created main html and css for ajax projects

---
+++ ajax-projects/README.md
@@ -0,0 +1,9 @@
+## Free Code Camp 
+### Intermediate Front End Development Projects
+
+There are simple projects for practicing in XHR & JSONP
+
+1. Show the Local Weather
+2. Random Quote Machine
+3. Wikipedia Viewer
+
--- ajax-projects/wikipedia-viewer/script.js
+++ ajax-projects/wikipedia-viewer/script.js
@@ -1,10 +1,8 @@
-'use strict';
-
-var getJsonp = function getJsonp() {
-  var jsonp = function jsonp(url, callback) {
-    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    var script = document.createElement('script');
-    window[callbackName] = function (data) {
+const getJsonp = () => {
+  const jsonp = (url, callback) => {
+    const callbackName = `jsonp_callback_${Math.round(100000 * Math.random())}`;
+    const script = document.createElement('script');
+    window[callbackName] = (data) => {
       delete window[callbackName];
       document.body.removeChild(script);
       callback(data);
@@ -14,38 +12,38 @@ var getJsonp = function getJsonp() {
     document.body.appendChild(script);
   };
 
-  var getWiki = function getWiki(data) {
-    var articleList = document.querySelector('.article-list');
+  const getWiki = (data) => {
+    const articleList = document.querySelector('.article-list');
     articleList.innerHTML = '';
     if (!data.query) {
-      var err = document.createElement('p');
+      const err = document.createElement('p');
       err.className = 'error';
       err.innerHTML = 'There were no results matching the query.';
       articleList.appendChild(err);
       return;
     }
 
-    var newData = data.query.pages;
-    for (var i in newData) {
+    const newData = data.query.pages;
+    for (const i in newData) {
       if ({}.hasOwnProperty.call(newData, i)) {
-        var link = document.createElement('a');
-        var title = document.createElement('h3');
-        var info = document.createElement('p');
+        const link = document.createElement('a');
+        const title = document.createElement('h3');
+        const info = document.createElement('p');
         link.className = 'wiki-info';
-        link.href = 'http://en.wikipedia.org/wiki?curid=' + newData[i].pageid;
+        link.href = `http://en.wikipedia.org/wiki?curid=${newData[i].pageid}`;
         link.target = '_blank';
         title.innerHTML = newData[i].title;
         info.innerHTML = newData[i].extract;
         articleList.appendChild(link);
         link.appendChild(title);
         link.appendChild(info);
-      };
+      }
     }
   };
 
-  var inputField = document.querySelector('.search-input');
+  const inputField = document.querySelector('.search-input');
 
-  var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + inputField.value + '&callback=JSON_CALLBACK';
+  const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${inputField.value}&callback=JSON_CALLBACK`;
 
   jsonp(url, getWiki);
 };
