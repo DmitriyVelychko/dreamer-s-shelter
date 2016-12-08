9681d733bd713c7d12e5b6ef7a58a3722224b21c
Dmitriy Velychko <dvelychko@sciforce.solutions>
2016-12-08 17:44:35 +0200

    Update code for random quote machine

--- src/app/random.quote.machine.js
+++ src/app/random.quote.machine.js
@@ -7,48 +7,46 @@ export default class RandomQuoteMachine extends Component {
     super(props);
   }
 
-  componentDidMount() {
-    var getQuote = function () {
-      var xhr = new XMLHttpRequest();
-      var rand = Math.round(Math.random() * 1000);
-      xhr.open('GET', "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + rand, true);
-      xhr.onload = function () {
-        var data = JSON.parse(xhr.responseText);
-        var content = data[0].content.replace(/[<p>,<\/p>]/g, "");
-        document.querySelector('.quote').innerHTML = content;
-        document.querySelector('.author').innerHTML = '- ' + data[0].title;
-        var href = document.querySelector('.quote').innerHTML;
-        if (href.length > 101) {
-          href = href.slice(0, 100);
-          href += "..."
+  _getQuote() {
+    const quoteId = Math.round(Math.random() * 1000);
+    fetch(`http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=${quoteId}`)
+      .then(
+        res => res.text()
+      )
+      .then(
+        body => {
+          const data = JSON.parse(body);
+          let quote = document.getElementById('quote-text');
+          quote.innerHTML = data[0].content.replace(/[<p>,<\/p>]/g, '');
+          document.getElementById('author').innerHTML = '- ' + data[0].title;
+          let slice = quote.innerHTML;
+          if (slice.length > 101) {
+            slice = slice.slice(0, 100);
+            slice += '... '
+          }
+          const tweet = `https://twitter.com/intent/tweet?text=${slice}(c) ${data[0].title}`.replace(/\;/g, '');
+          document.getElementById('tweet').href = tweet;
         }
-        document.querySelector('.tweet').href = "https://twitter.com/intent/tweet?text=" + href + " (c) " + data[0].title;
-      };
-      xhr.send();
-    }
+      );
+  }
 
-    var openURL = function () {
-      event.preventDefault();
-      var str = window.open(this.href, 'Twitter', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-    }
-    document.querySelector('#next-quote').addEventListener('click', getQuote, false);
-    document.querySelector('.tweet').addEventListener('click', openURL, true);
-
-    getQuote();
-
+  componentDidMount() {
+    this._getQuote();
+    document.getElementById('next-quote').addEventListener('click', this._getQuote);
   }
 
   render() {
     return <div>
-      <Link to="/" className="link link-home">Home</Link>
-      <div className="title">Random Quote Machine</div>
-      <div className="wrapper">
-        <blockquote className="quote"></blockquote>
-        <div className="author"></div>
-        <div className="navigate"><a href="#" className="tweet">
+      <Link to='/' className='link link-home'>Home</Link>
+      <div className='title'>Random Quote Machine</div>
+      <div className='wrapper'>
+        <blockquote id='quote-text'></blockquote>
+        <div id='author'></div>
+        <div className='navigate'>
+          <a href='' target='_blank' id='tweet'>
           <i></i>
           <span>Tweet</span></a>
-          <button id="next-quote" className="btn btn-long">Next Quote</button>
+          <button id='next-quote' className='btn btn-long'>Next Quote</button>
         </div>
       </div>
     </div>
